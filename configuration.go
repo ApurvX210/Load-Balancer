756818cc8327c2b664e354c62efc9bf761e467a3
ApurvX210 <apurvsharma210@gmail.com>
2025-12-29 23:46:39 +0530

    Added config.go file for mapping yml to go code

--- configuration.go
+++ configuration.go
@@ -1,8 +1,40 @@
 package main
 
-type Config struct{
-	server 			map[string] string
-	load_balancer	map[string] string
-	backends		map[string] map[string]string
-	health_check	map[string] string
+import (
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
+type Config struct {
+	Server struct {
+		Port         string `yaml:"port"`
+		ReadTimeout  string `yaml:"readTimeout"`
+		WriteTimeout string `yaml:"rriteTimeout"`
+	} `yaml:"server"`
+	LoadBalancer struct {
+		Algorithm string `yaml:"algorithm"`
+	} `yaml:"load_balancer"`
+	Backends []struct {
+		Url          string `yaml:"url"`
+		InitialAlive bool   `yaml:"initial_alive"`
+	} `yaml:"backends"`
+	HealthCheck struct {
+		Interval string `yaml:"interval"`
+		Endpoint string `yaml:"endpoint"`
+	} `yaml:"health_check"`
+}
+
+func parseYaml(path string) (*Config,error) {
+	yamlFile, err := os.ReadFile(path)
+	if err != nil{
+		return nil,err
+	}
+	var config Config;
+	err = yaml.Unmarshal(yamlFile,&config)
+	if err != nil{
+		return nil,err
+	}
+
+	return &config,nil
 }
